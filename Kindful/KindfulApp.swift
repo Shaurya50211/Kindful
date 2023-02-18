b8847ca364fde890fd826fc30e9bdf876c056037
Shaurya50211 <shaurya50211@gmail.com>
2023-02-18 07:48:02 -0500

    Initial Commit

---
+++ Kindful/KindfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindfulApp.swift
+//  Kindful
+//
+//  Created by Shaurya Gupta on 2023-02-18.
+//
+
+import SwiftUI
+
+@main
+struct KindfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
