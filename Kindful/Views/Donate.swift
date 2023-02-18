40097092b83b803374bafe8466215d9dc48d569e
G.K.Naidu <107152201+GK-naidu@users.noreply.github.com>
2023-02-19 01:16:57 +0530

    completed to add webView in the donate screen of the app

--- Kindful/Views/Donate.swift
+++ Kindful/Views/Donate.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct Donate: View {
+    @State private var showWebView = false
+    private let urlString: String = "https://donate.red.org/give/342913/?_ga=2.125558026.1296450177.1676741554-767929606.1676741553#!/donation/checkout"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            GeometryReader { geometry in
+                WebView(url: URL(string: urlString)!).frame(width: geometry.size.width,height: geometry.size.height)
+                
+            }
+        }
     }
+}
+
+struct WebView : UIViewRepresentable {
+    var url : URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+    
+    
+    
 }
 
 struct Donate_Previews: PreviewProvider {
